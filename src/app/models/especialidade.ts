fe21e1c109229a13592cfbcd91c780cea5288cd5
Gisele Goncalves <giselegoncalves@gmail.com>
2020-11-07 20:40:04 -0300

    0711 05

--- src/app/models/especialidade.ts
+++ src/app/models/especialidade.ts
@@ -1,9 +1,7 @@
 export class especialidade{
-    private codigo :number;
     private nome :string;
-
-    constructor(pCodigo :number, pNome :string){
-        this.codigo = pCodigo;
+    
+    constructor(pNome :string){
         this.nome = pNome;
     }
 
--- src/app/models/farmacia.ts
+++ src/app/models/farmacia.ts
@@ -1,11 +1,9 @@
 export class farmacia{
-    private codigo :number;
     private nome :string;
     private status :string;
     private codBairro :number;
 
-    constructor(pCodigo :number, pNome :string, pStatus :string, pCodBairro :number){
-        this.codigo = pCodigo;
+    constructor(pNome :string, pStatus :string, pCodBairro :number){
         this.nome = pNome;
         this.status = pStatus;
         this.codBairro = pCodBairro;
